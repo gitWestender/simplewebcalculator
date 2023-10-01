093557a389db1b3209a66da7cb3c455d166edf24
Westender <javaxwestender@gmail.com>
2023-10-01 14:50:14 +0300

    Add functions

--- src/main/java/com/homelearning/simplecalculator/ISimplecalculatorService.java
+++ src/main/java/com/homelearning/simplecalculator/ISimplecalculatorService.java
@@ -1,4 +1,9 @@
 package com.homelearning.simplecalculator;
 
-public class ISimplecalculatorService {
+public interface ISimplecalculatorService {
+    String welcome();
+    String addition(int num1, int num2);
+    String subtraction(int num1, int num2);
+    String multiplication(int num1, int num2);
+    String division(int num1, int num2);
 }
--- src/main/java/com/homelearning/simplecalculator/SimplecalculatorService.java
+++ src/main/java/com/homelearning/simplecalculator/SimplecalculatorService.java
@@ -3,5 +3,24 @@ package com.homelearning.simplecalculator;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SimplecalculatorService {
+public class SimplecalculatorService implements ISimplecalculatorService{
+    public String welcome() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public String addition(int num1, int num2) {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
+    }
+
+    public String subtraction(int num1, int num2) {
+        return num1 + " - " + num2 + " = " + (num1 - num2);
+    }
+
+    public String multiplication(int num1, int num2) {
+        return num1 + " * " + num2 + " = " + (num1 * num2);
+    }
+
+    public String division(int num1, int num2) {
+        return num1 + " / " + num2 + " = " + ((float)num1 / (float)num2);
+    }
 }
