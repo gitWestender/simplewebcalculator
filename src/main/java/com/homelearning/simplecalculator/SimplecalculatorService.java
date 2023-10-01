8acfc5f3f9e58b4fe898676404028f6daf4a6bd1
Westender <javaxwestender@gmail.com>
2023-10-01 15:30:23 +0300

    Complete

--- src/main/java/com/homelearning/simplecalculator/SimplecalculatorService.java
+++ src/main/java/com/homelearning/simplecalculator/SimplecalculatorService.java
@@ -1,9 +1,10 @@
 package com.homelearning.simplecalculator;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Service
-public class SimplecalculatorService implements ISimplecalculatorService{
+public class SimplecalculatorService implements ISimplecalculatorService {
     public String welcome() {
         return "Добро пожаловать в калькулятор";
     }
@@ -21,6 +22,10 @@ public class SimplecalculatorService implements ISimplecalculatorService{
     }
 
     public String division(int num1, int num2) {
-        return num1 + " / " + num2 + " = " + ((float)num1 / (float)num2);
+        if (num2 == 0)
+            return "На ноль делить нельзя";
+        else
+            return num1 + " / " + num2 + " = " + (num1 / (float) num2);
     }
+
 }
